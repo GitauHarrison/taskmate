4001fc11675b45c14c00d03e0ea29017b3d7c324
harry <harrisongitau3@gmail.com>
2023-07-21 15:58:42 +0300

    Dev: Remove some files not used in this project setup

--- boot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source venv/bin/activate
-exec gunicorn --access-logfile - --error-logfile - 'web:create_app()'
--- gunicorn.conf.py
+++
@@ -1,5 +0,0 @@
-import multiprocessing
-
-bind = "0.0.0.0:5001"
-workers = multiprocessing.cpu_count() * 2 + 1
-print("number of workers:", workers)
